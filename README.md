c67b4c61a3599ad5b5f4877813ada6c833f92d6d
root <root@raspberry.local>
2023-01-06 15:51:21 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# [HTML MSN] Windows Live Messenger Web
+Microsoft's Windows Live Messenger brought back in HTML and [TailwindCSS](https://tailwindcss.com)
+
+
+# Features
+- Chatting over the web
+- Retro sounds
+- Spotify Integration
+- More...
+
+## Configuration
+ Work in progress
+
+## Setup
+ Work in progress...
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        nudge: {
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(3px, 3px)" },
+          "50%": { transform: "translate(-3px, 5px)" },
+          "75%": { transform: "translate(5px, 8px)" },
+          "100%": { transform: "translate(0, 0)" },
+        },
+      },
+      animation: {
+        nudge: "nudge 0.3s ease-out infinite",
+      },
+      backgroundImage: {
+        xp: "url('../images/winxp.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
